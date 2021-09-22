cc3d6450d4778d25cfbbf7db6c7c5b058e8b2fc3
huyi <792743514@qq.com>
2021-09-22 21:17:33 +0800

    完成了主页功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -5,4 +5,22 @@ $.ajaxPrefilter(function(options) {
     console.log(options.url);
     //拼接请求的真正路径
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url;
+
+    //统一为有权限的接口设置headers请求头，省的一个一个的写
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+
+    //全局统一挂载 complete 回调函数
+    options.complete = function(res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            //1 清除本地存储的token
+            localStorage.removeItem('token');
+            // 2 重新跳转登录界面
+            location.href = '/login.html';
+        }
+    }
 })
